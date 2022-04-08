3dca8adcfa44a73f9114d9c09a850ed6201a93ab
CeciliaTeodorani <cecilia.teodorani@studio.unibo.it>
2022-04-08 15:52:37 +0200

    Modify tests

--- src/pcd/ass01/conc/test/CalculationsTest.java
+++ src/pcd/ass01/conc/test/CalculationsTest.java
@@ -3,7 +3,6 @@ package pcd.ass01.conc.test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,6 @@ import pcd.ass01.conc.Simulator;
 import pcd.ass01.seq.SequentialSimulator;
 import pcd.ass01.utils.*;
 
-// test with only 2 or 3 bodies changing Simulator and SequentialSimulator
 public class CalculationsTest {
 	
 	private Simulator concurrentSimulator;
@@ -22,7 +20,7 @@ public class CalculationsTest {
 	public void setUp() {
 		BodyGenerator bg = new BodyGenerator();
 		Boundary bounds = new Boundary(-6.0, -6.0, 6.0, 6.0);
-		ArrayList<Body> bodySet = bg.generateBodies(100, bounds);
+		ArrayList<Body> bodySet = bg.generateBodies(2, bounds);
 		this.concurrentSimulator = new Simulator(bodySet, bounds);
 		this.sequentialSimulator = new SequentialSimulator(bodySet, bounds);
 	}
@@ -30,13 +28,10 @@ public class CalculationsTest {
 	// N steps = 1000
 	@Test
 	public void testExecuteThousand() {
-		System.out.println("uno");
 		this.concurrentSimulator.execute(1000);
-		System.out.println("due");
 		this.sequentialSimulator.execute(1000);
-		System.out.println("tre");
 		
-		//this.checkCalculations(this.concurrentSimulator, this.sequentialSimulator);
+		this.checkCalculations(this.concurrentSimulator, this.sequentialSimulator);
 	}
 	
 	// N steps = 10000
@@ -62,12 +57,12 @@ public class CalculationsTest {
 		if(concSimulator.getBodies().size() == seqSimulator.getBodies().size()) {
 			for(int i = 0; i < concSimulator.getBodies().size(); i++) {
 				// check position calculation accuracy
-				assertEquals(concSimulator.getBodies().get(i).getPos().getX(), seqSimulator.getBodies().get(i).getPos().getX(), 0.00001);
-				assertEquals(concSimulator.getBodies().get(i).getPos().getY(), seqSimulator.getBodies().get(i).getPos().getY(), 0.00001);
+				assertEquals(concSimulator.getBodies().get(i).getPos().getX(), seqSimulator.getBodies().get(i).getPos().getX(), 0.0);
+				assertEquals(concSimulator.getBodies().get(i).getPos().getY(), seqSimulator.getBodies().get(i).getPos().getY(), 0.0);
 				
 				// check velocity calculation accuracy
-				assertEquals(concSimulator.getBodies().get(i).getVel().getX(), seqSimulator.getBodies().get(i).getVel().getX(), 0.00001);
-				assertEquals(concSimulator.getBodies().get(i).getVel().getY(), seqSimulator.getBodies().get(i).getVel().getY(), 0.00001);
+				assertEquals(concSimulator.getBodies().get(i).getVel().getX(), seqSimulator.getBodies().get(i).getVel().getX(), 0.0);
+				assertEquals(concSimulator.getBodies().get(i).getVel().getY(), seqSimulator.getBodies().get(i).getVel().getY(), 0.0);
 			}
 		}
 	} 
